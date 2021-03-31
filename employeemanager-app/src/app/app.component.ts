1af4c7043938bcf344c696fd64d7d8b3c45270ad
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-03-31 17:23:43 -0300

    Add the missing bindings so that edit, delete and search work.

--- employeemanager-app/src/app/app.component.ts
+++ employeemanager-app/src/app/app.component.ts
@@ -41,9 +41,11 @@ export class AppComponent implements OnInit {
         button.setAttribute('data-target', '#addEmployeeModal');
         break;
       case 'edit':
+        this.editEmployee = employee;
         button.setAttribute('data-target', '#updateEmployeeModal');
         break;
       case 'delete':
+        this.deleteEmployee = employee;
         button.setAttribute('data-target', '#deleteEmployeeModal');
         break;
     }
@@ -67,11 +69,40 @@ export class AppComponent implements OnInit {
   }
 
   public onUpdateEmloyee(employee: Employee): void {
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.message);
+      }
+    );
   }
 
   public onDeleteEmloyee(employeeId: number): void {
+    this.employeeService.deleteEmployee(employeeId).subscribe(
+      (response: void) => {
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.message);
+      }
+    );
   }
 
   public searchEmployees(key: string): void {
+    const results: Employee[] = [];
+    for (const employee of this.employees) {
+      if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if (results.length === 0 || !key) {
+      this.getEmployees();
+    }
   }
 }
